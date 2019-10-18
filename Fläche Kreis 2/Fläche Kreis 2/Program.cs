d5de1f3a3a43b12f85e2445af3fe1c1f8887846a
SvenWellhausen <56021914+SvenWellhausen@users.noreply.github.com>
2019-10-18 21:27:46 +0200

    Fläche Kreis

---
+++ Fläche Kreis 2/Fläche Kreis 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fläche_Kreis_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Berechnung Kreisfläche");
+            Console.WriteLine("Kreisdurchmesser in mm eingeben:");
+            String r = Console.ReadLine();
+            double zahl = Convert.ToDouble(r);
+            double ergebnis = zahl * zahl;
+            double ergebnis2 = ergebnis * 0.785;
+            Console.WriteLine("Ergebnis in mm^2 :" + ergebnis2);
+            Console.ReadKey();
+        }
+    }
+}
