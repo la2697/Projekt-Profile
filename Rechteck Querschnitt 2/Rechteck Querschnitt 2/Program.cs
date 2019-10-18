5cdc319b1f8ab24c3a9848ec5d98d7c157f157b7
SvenWellhausen <56021914+SvenWellhausen@users.noreply.github.com>
2019-10-18 21:34:25 +0200

    Rechteck Querschnitt

---
+++ Rechteck Querschnitt 2/Rechteck Querschnitt 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rechteck_Querschnitt_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rechteck Querschnitt Berechnung");
+            Console.WriteLine("Länge a in mm eingeben");
+            String a = Console.ReadLine();
+            double zahl = Convert.ToDouble(a);
+            Console.WriteLine("Breite b in mm eingeben");
+            String b = Console.ReadLine();
+            double zahlb = Convert.ToDouble(b);
+
+            double Ergebnis = zahl * zahlb;
+            Console.WriteLine("Ergebnis in mm^2:" + Ergebnis);
+            Console.ReadKey();
+        }
+    }
+}
