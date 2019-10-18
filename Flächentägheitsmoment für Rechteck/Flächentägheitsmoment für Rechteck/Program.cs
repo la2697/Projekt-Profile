22d69293b30d4a7684c12dce4ef9257f699b9608
Lo2064 <56021858+Lo2064@users.noreply.github.com>
2019-10-18 13:57:10 +0200

    Flaechentragsmoment

---
+++ Flächentägheitsmoment für Rechteck/Flächentägheitsmoment für Rechteck/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flächentägheitsmoment_für_Rechteck
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Berechnung der Flaechentaegheitsmoment fuer Rechteck :");
+            Console.WriteLine("Eingabe der Laenge:");
+            String Laenge = Console.ReadLine( ); 
+            Double a = Convert.ToDouble(Laenge);
+            Console.WriteLine("Eingabe der Breite:");
+            String Breite = Console.ReadLine();
+            Double b = Convert.ToDouble(Breite);
+            double Flächenträgheitsmoment  = a * Math.Pow(b, 3)/12;
+            Console.WriteLine("Ausgabe des Flaechentraegheitsmoments fuer x-Achse:");
+            Console.WriteLine(Flächenträgheitsmoment );
+            double Flächenträgheitsmoment2 = b * Math.Pow(a, 3) / 12;
+            Console.WriteLine("Ausgabe des Flaechentraegheitsmoments fuer y-Achse:");
+            Console.WriteLine(Flächenträgheitsmoment2);
+        }
+    }
+}
