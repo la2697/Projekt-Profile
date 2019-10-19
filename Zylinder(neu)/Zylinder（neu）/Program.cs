3ec77138ad336461c391ed27ec1cdf908ea0f9ff
ya2149 <56022595+ya2149@users.noreply.github.com>
2019-10-19 12:01:08 +0200

    Zylinder(neu)

---
+++ Zylinder(neu)/Zylinder（neu）/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zylinder1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte eingeben das Radius");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bitte eingeben die laenge");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double aa;
+            aa = 3.14159265*a*a*b;
+            Console.WriteLine("Volumen ist {0}", aa);
+            double bb;
+            bb = aa * 2.7;
+            Console.WriteLine("Gewicht ist {0}", bb);
+            Console.ReadKey();
+        }
+    }
+}
