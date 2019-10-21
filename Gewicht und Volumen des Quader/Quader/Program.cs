bce09ebf3d9ef1a4786af10aed41401a66034d54
ya2149 <56022595+ya2149@users.noreply.github.com>
2019-10-21 16:00:24 +0200

    Gewicht und Volumen des Quader

---
+++ Gewicht und Volumen des Quader/Quader/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte eingeben die Breite");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bitte eingeben die laenge");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bitte eingeben die Hoehe");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double aa;
+            aa = a * b * c;
+            Console.WriteLine("Volumen ist {0}", aa);
+            double bb;
+            bb = aa * 2.7;
+            Console.WriteLine("Gewicht ist {0}", bb);
+            Console.ReadKey();
+        }
+    }
+}
