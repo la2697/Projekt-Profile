a4e3f49a06ad6f1b7f3a9bcab2d8c3793694befc
la2697 <56022392+la2697@users.noreply.github.com>
2019-10-22 08:52:45 +0200

    Oberfläche

---
+++ Berechnungen für Profile/Berechnungen für Profile/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berechnungen_für_Profile
+{
+    class Program
+    {
+          
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fläche Kreis2");
+            Console.WriteLine("Rechtecke Querschnitt");
+            Console.WriteLine("Quader");
+            Console.WriteLine("Zylinder");
+            Console.WriteLine("Flächenträgheitsmoment für Recteck");
+            Console.WriteLine("Flächenträgheitsmoment für Kreis");
+            Console.ReadKey();
+
+
+
+        }
+
+        static public Flächenträgheitsmoment für Rechteck()
+        {
+
+        }
+
+        static public Flächenträgheitsmoment für Kreis()
+        {
+
+        }
+
+        static public Fläche Kreis2()
+        {
+
+        }
+
+        static public Rechteck Querschnitt()
+        {
+
+        }
+        
+        static public Quader()
+        {
+
+        }
+
+        static public  Zylinder()
+        {
+
+        }
+            
+    }
+}
