2343139f4cc66447b608d7d7efb56e506ee10ce5
ya2149 <56022595+ya2149@users.noreply.github.com>
2019-10-29 15:53:48 +0100

    GGT

---
+++ GGT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                c = a % b;
+                a = b;
+                b = c;
+
+            } while ( c != 0);
+            Console.WriteLine("{0}", a);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
